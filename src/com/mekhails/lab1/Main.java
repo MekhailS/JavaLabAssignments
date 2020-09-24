ddc0f2fc7709ffe8f5e6deeb0a2edd7ee654cf03
MikhailS <shagvaliev.mikhail@gmail.com>
2020-09-24 23:26:13 +0300

    Add test for testing (configs and in.txt)

--- src/com/mekhails/lab1/Main.java
+++ src/com/mekhails/lab1/Main.java
@@ -4,8 +4,14 @@ public class Main
 {
     public static void main(String[] Args)
     {
-        Manager mng1 = new Manager("config.txt");
-        Manager mng2 = new Manager("config2.txt");
+        if (Args == null || Args.length == 0) {
+            System.out.println("Error: empty arguments of command prompt");
+        }
+
+        String configFilename = Args[0];
+
+        Manager mng1 = new Manager("src/com/mekhails/lab1/config1.txt");
+        Manager mng2 = new Manager("src/com/mekhails/lab1/config2.txt");
 
         mng1.Run();
         mng2.Run();
