0f8ac9f9fcadcaed38bacb3986405e514b74de5a
MikhailS <shagvaliev.mikhail@gmail.com>
2020-11-02 14:50:46 +0300

    add lab2 which was commited to course instructor

--- src/com/mekhails/lab2/Main.java
+++ src/com/mekhails/lab2/Main.java
@@ -1,17 +1,17 @@
 package com.mekhails.lab2;
 
+import java.util.logging.Level;
+
 public class Main
 {
     public static void main(String[] Args)
     {
-        /*if (Args == null || Args.length == 0) {
+        if (Args == null || Args.length == 0) {
             Log.LOGGER.log(Level.SEVERE, Log.ERROR.COMMAND_PROMPT.name);
             return;
         }
 
-        String configFilename = Args[0];*/
-
-        String configFilename = "src\\com\\mekhails\\lab2\\managerConfig.txt";
+        String configFilename = Args[0];
 
         Manager mng = new Manager(configFilename);
 
